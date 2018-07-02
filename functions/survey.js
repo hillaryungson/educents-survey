a6ff7dda61d9a55102c5fef7b141947287af6deb
hillaryungson <hillaryungson@gmail.com>
2018-07-01 21:26:46 -0700

    Update survey.js

--- functions/survey.js
+++ functions/survey.js
@@ -1,6 +1,21 @@
 exports.handler = function(event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Hello, World"
-  });
+  var myArray = [
+    "http://bit.ly/2IIiRdr",
+    "http://bit.ly/2KA5Ibi",
+    "http://bit.ly/2tX8UTM"
+  ];
+
+  var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
+  console.log("redirect URL", randomItem)
+  
+  const response = {
+    statusCode: 301,
+    headers: {
+      Location: randomItem,
+      // Set no cache
+      'Cache-Control': 'no-cache'
+    }
+  }
+
+  return callback(null, response)
 };
